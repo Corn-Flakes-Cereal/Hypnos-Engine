2bb215350d953f8c01b1d019dcbf1d20c78c9fdc
batBoi <msmitzel@gmail.com>
2022-12-11 02:35:09 -0500

    Add project files.

---
+++ Hypnos Engine/Hypnos Engine.cpp
@@ -0,0 +1,12 @@
+﻿// Hypnos Engine.cpp : Defines the entry point for the application.
+//
+
+#include "Hypnos Engine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
